38430df07963ce6f8001fc19dffb0c3a39dce6a6
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-09-20 12:09:24 -0700

    adding Cupcakes model and route to see all cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
+from models import Cupcake, db, connect_db
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+@app.get("/api/cupcakes")
+def list_all_cupcakes():
+    """Return a list of all cupcakes"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
--- models.py
+++ models.py
@@ -1,1 +1,48 @@
 """Models for Cupcake app."""
+
+from typing import Optional
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    __tablename__ = "cupcakes"
+
+    id =  db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True)
+
+    flavor = db.Column(db.Text, 
+                        nullable=False)
+    
+    size = db.Column(db.Text, 
+                        nullable = False)
+
+    rating = db.Column(db.Integer, 
+                        nullable= False)
+
+    image = db.Column(db.Text, 
+                        nullable = False,
+                        default = 'https://tinyurl.com/demo-cupcake')
+
+    def serialize(self):
+        """Serialize to dictionary"""
+
+        return {
+            "id": self.id, 
+            "flavor": self.flavor,
+            "size": self.size, 
+            "rating": self.rating,
+            "image": self.image
+        }
