05f0175e587f4d0238c695c76bd978444e1c52af
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-09-20 12:33:03 -0700

    added additional routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 # from flask_debugtoolbar import DebugToolbarExtension
 from models import Cupcake, db, connect_db
 
@@ -23,4 +23,38 @@ def list_all_cupcakes():
     serialized = [c.serialize() for c in cupcakes]
 
     return jsonify(cupcakes=serialized)
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def return_cupcake_detail(cupcake_id):
+    """ Returns a dictionary of cupcake information """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """ Creates a new cupcake and respond with JSON like: 
+    {cupcake: {id, flavor, size, rating, image}}. 
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(
+        flavor=flavor, 
+        size=size, 
+        rating=rating, 
+        image=image)
+    
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized),201)
+
 
